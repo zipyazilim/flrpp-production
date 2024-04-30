285780710891412715211686fdb2030d66336a4b
zipyazilim <115250607+zipyazilim@users.noreply.github.com>
2024-04-30 02:25:07 +0200

    Update index.tsx

--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -19,14 +19,14 @@ const Navigation = ({ user }: Props) => {
           height={40}
           alt="plur logo"
         />
-        <span className="text-xl font-bold"> Plura.</span>
+        <span className="text-xl font-bold"> Flura.</span>
       </aside>
       <nav className="hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
         <ul className="flex items-center justify-center gap-8">
-          <Link href={'#'}>Pricing</Link>
-          <Link href={'#'}>About</Link>
-          <Link href={'#'}>Documentation</Link>
-          <Link href={'#'}>Features</Link>
+          <Link href={'#'}></Link>
+          <Link href={'#'}></Link>
+          <Link href={'#'}></Link>
+          <Link href={'#'}></Link>
         </ul>
       </nav>
       <aside className="flex gap-2 items-center">
